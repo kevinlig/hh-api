eef3b65a1e64c87914313e8635657527ef655b3e
Tony Chen <tonychen091@gmail.com>
2014-04-13 07:53:22 -0400

    edits

--- core.js
+++ core.js
@@ -5,19 +5,38 @@ var evergreenDoc = angular.module('evergreenDoc', []);
 function mainController($scope, $http, $timeout) {
 	$scope.formData = {};
 
+	var initialList= [];
 
-
+	var groupings={}; 
 
 	// when landing on the page, get all todos and show them
 
-	
+	$scope.openDetails = function(patient_id){
+		console.log('someting');
+		$scope.groupingList = groupings[patient_id];
+	};
 
     (function tick() {
        $http({method: 'GET', url: '/statuses'})
 		.success(function(data){
-			$scope.statuses = data;
-			$timeout(tick, 1000);
-			console.log("yESSS!");
+			$scope.statuses = data.slice().reverse();
+			
+			for (var i = 0; i < data.length; i++) {
+				if (groupings[data[i].patient_id] === undefined){
+					groupings[data[i].patient_id] =[];
+					initialList.push(data[i])
+
+				} else {
+					groupings[data[i].patient_id].push(data[i]);
+				}
+				
+				
+
+			};
+			$scope.initialList = initialList;
+			console.log(groupings);
+			// $timeout(tick, 1000);
+			
 		});
             
         
